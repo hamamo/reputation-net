2e5d871db41a8d309145780c1a65e5f433018e84
Hans-Martin Mosner <hmm@heeg.de>
2022-06-24 14:52:31 +0200

    Fixed Migration

--- migrations/20220624063436_remove_entity_3+4.sql
+++ migrations/20220624063436_remove_entity_3+4.sql
@@ -1,3 +1,24 @@
 -- remove fields which are not used
-alter table statement drop entity_3;
-alter table statement drop entity_4;
+COMMIT; -- transaction must be committed for pragmas to really work
+PRAGMA foreign_keys=off;
+PRAGMA legacy_alter_table=on;
+BEGIN TRANSACTION;
+
+DROP INDEX idx_cidr_min;
+DROP INDEX idx_cidr_max;
+DROP INDEX idx_entity_1;
+
+ALTER TABLE statement RENAME TO old_statement;
+
+CREATE TABLE statement AS
+    SELECT id, name, entity_1, entity_2, cidr_min, cidr_max, last_used, last_weight
+    FROM old_statement;
+
+create index if not exists idx_cidr_min on statement(cidr_min);
+create index if not exists idx_cidr_max on statement(cidr_max);
+create index if not exists idx_entity_1 on statement(entity_1);
+
+DROP TABLE old_statement;
+
+PRAGMA foreign_keys=on;
+PRAGMA legacy_alter_table=off;
